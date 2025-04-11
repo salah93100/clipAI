ef619bfab4ac44d14b6020553d9b3ca30fcf1b51
salah93100 <salah.elbouhali@digityourdream.fr>
2025-04-11 12:32:48 +0200

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN apk add --no-cache libc6-compat ffmpeg
 WORKDIR /app
 
 # Copie des fichiers package.json et package-lock.json
-COPY package.json pnpm-lock.yaml* .env ./
+COPY package.json pnpm-lock.yaml* .env.production ./
 RUN npm install -g pnpm && pnpm i --frozen-lockfile
 
 # Étape de construction (build)
--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -26,8 +26,8 @@ export async function GET(request: Request) {
     
     // Créer un client Supabase classique
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_ANON_KEY!
     );
     
     // Échange du code contre une session
